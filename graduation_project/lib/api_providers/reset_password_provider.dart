9464e694e9cf46606960689def2c51d59c6215b5
SSSomia <smiasrour>
2025-04-23 17:12:28 +0200

    go to login page after reset password

--- graduation_project/lib/api_providers/reset_password_provider.dart
+++ graduation_project/lib/api_providers/reset_password_provider.dart
@@ -9,13 +9,15 @@ class ResetPasswordProvider with ChangeNotifier {
   bool get isAuthenticated => _isAuthenticated;
 
   // Login method
-  Future<void> resetPassword(String email, String token0,  String password) async {
+  Future<void> resetPassword(
+      String email, String token0, String password) async {
     // Call the API to authenticate the user
     final result = await ApiService().resetPassword(email, token0, password);
-    if (result != null && result.containsKey('token')) {
-      _token = result['token'];
-      _isAuthenticated = true;
-      notifyListeners(); // Notify listeners (UI) that the state has changed
-    }
+    _isAuthenticated = true;
+    notifyListeners(); // Notify listeners (UI) that the state has changed
+    // if (result != null && result.containsKey('token')) {
+    //   _token = result['token'];
+    //   _isAuthenticated = true;
+    // }
   }
 }
